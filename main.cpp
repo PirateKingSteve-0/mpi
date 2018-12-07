b16b642aacaef236b05c25aeec9e9d037c9aaf24
Austin Suarez <amsuarez@outlook.com>
2018-12-07 12:33:58 -0800

    going with vector instead of double pointer

--- main.cpp
+++ main.cpp
@@ -1,28 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <vector>
 #include <mpi.h>
 #include <iostream>
 #include <assert.h>
 #include <fstream>
 using namespace std;
 // Creates an array of random numbers. Each number has a value from 0 - 1
-int **create_matrix(int matrixWidth,ifstream &inputFile) {
-  int **matrix;
-  int  num; 
-  //inputs to matrix
-  matrix = new int*[matrixWidth];
+void create_matrix(vector<vector<int> > &matrix,int matrixWidth,ifstream &inputFile) {
+  int num;
+
   for (int row =0; row < matrixWidth; row++){
-      matrix[row] = new int[matrixWidth];
+      vector<int> singleRow;
+      matrix.push_back(singleRow);
       for(int column = 0; column < matrixWidth; column++){
         inputFile >> num;
-        matrix[row][column] = num;
+        matrix[row].push_back(num);
       }
   }
-  return matrix;
 }
 
-void displayMatrix(int **matrix, int matrixWidth){
+void displayMatrix(vector<vector<int> > matrix, int matrixWidth){
   for(int row = 0; row < matrixWidth; row++){
     for(int col = 0; col < matrixWidth; col++){
       cout << matrix[row][col] << " ";
@@ -31,34 +29,55 @@ void displayMatrix(int **matrix, int matrixWidth){
   }
 } 
 
-void displaySubMatrix(int *subMatrix, int matrixWidth){
-  for(int number = 0; number < matrixWidth; number++){
-      cout << subMatrix[number] << " ";
-      if(number%matrixWidth == 0){
-        cout << endl;
-      }
-  }
-}
+// void displaySubMatrix(int *subMatrix, int matrixWidth){
+//   int matrixSize= matrixWidth+matrixWidth;
+//   cout << endl << "Submatrix: " << endl;
+//   for(int number = 0; number < matrixSize; number++){
+//     if(number+number == matrixSize){
+//         cout<<endl;
+//     }
+//     cout << subMatrix[number] << " ";
+//   }
+//   cout << endl;
+// }
 
-// Computes the average of an array of numbers
-int computeSum(int* array, int num_elements) {
-  int sum = 0;
-  cout << "num_elements: " << num_elements << endl;
-  cout << "numbers in array: ";
-  for (int index = 0; index < num_elements; index++) {
-    sum += array[index];
-  }
-  cout<< endl; 
-  cout << "sum: " << sum << endl;
-  return sum ;
-}
+// // Computes the average of an array of numbers
+// int computeSum(int* array, int num_elements) {
+//   int sum = 0;
+//   cout << "num_elements: " << num_elements << endl;
+//   cout << "numbers in array: ";
+//   for (int index = 0; index < num_elements; index++) {
+//     sum += array[index];
+//   }
+//   cout<< endl; 
+//   cout << "sum: " << sum << endl;
+//   return sum ;
+// }
+
+// void getSubMatrix(int **matrix, int startRow,int startCol ,int subMatrixSize){
+//   vector<int> subMatrix(subMatrrixSize);
+//   int  index;
+//   index = 0;
+//   cout << "startRow: " << startRow << " startCol: "<< startCol <<endl;
+//   for (int row = startRow; row < subMatrixSize; row++){
+//     cout<< "numbers being inserted: ";
+//     for(int col = startCol; col < subMatrixSize; col++){
+//       cout << matrix[row][col] << ' ';
+//       subMatrix[index] = matrix[row][col];
+//       index++;
+//     }
+//     cout << endl;
+//   }
+
+//   displaySubMatrix(subMatrix, subMatrixSize);
+// }
 
 int main(int argc, char** argv) {
-
   ifstream inputFile;
   int matrixWidth;
+  int world_rank;
+  int world_size;
   string line;
-
 
   if (argc != 2){
     cout << "error" << endl;
@@ -66,33 +85,33 @@ int main(int argc, char** argv) {
     exit(1);
   }
 
-  int num_elements_per_proc = atoi(argv[1]);
-  // Seed the random number generator to get different results each time
-  srand(time(NULL));
-
+  int sizeOfSubMatrix = atoi(argv[1]);
   MPI_Init(NULL, NULL);
-
-  int world_rank;
-
   MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-  int world_size;
   MPI_Comm_size(MPI_COMM_WORLD, &world_size);
   
   // Create a random array of elements on the root process. Its total
   // size will be the number of elements per process times the number
   // of processes
-  int **matrix = NULL;
-  cout << endl << "process: " << world_rank << endl;
+  vector<vector <int> > matrix;
   if (world_rank == 0) {
-    inputFile.open("./data.txt");
-        
+    inputFile.open("./data.txt");      
     //gets the size of the matrix
     getline(inputFile, line);
     sscanf(line.c_str(), "%d", &matrixWidth);
-    cout << "size of the array: " << matrixWidth << endl;
-    matrix = create_matrix(matrixWidth, inputFile);
-    displayMatrix(matrix, matrixWidth);
-   
+    create_matrix(matrix,matrixWidth, inputFile);
+    displayMatrix(matrix,matrixWidth);
+
+    cout << endl;
+    cout << "Sub Matrix list: "<< endl; 
+    // for(int row = 0; row < matrixWidth; row += sizeOfSubMatrix){
+    //   for(int col = 0; col < matrixWidth; col += sizeOfSubMatrix){
+    //     getSubMatrix(matrix,row,col,sizeOfSubMatrix);
+    //     cout << endl << endl;
+    //   }
+    //   displayMatrix(matrix, matrixWidth);
+    //   cout << endl;
+    // }
   }
   else{ 
     
