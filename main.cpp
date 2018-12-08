f7d3f9e396d9e14ec9d2499e73a8d6b4e3d2e09c
Austin Suarez <amsuarez@outlook.com>
2018-12-07 16:37:08 -0800

    changed pointers to vectors

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <assert.h>
 #include <fstream>
 using namespace std;
+
 // Creates an array of random numbers. Each number has a value from 0 - 1
 void create_matrix(vector<vector<int> > &matrix,int matrixWidth,ifstream &inputFile) {
   int num;
@@ -29,57 +30,55 @@ void displayMatrix(vector<vector<int> > matrix, int matrixWidth){
   }
 } 
 
-void displaySubMatrix(int subMatrix[], int matrixWidth){
+void displaySubMatrix(vector<int> subMatrix, int matrixWidth){
   int matrixSize= matrixWidth+matrixWidth;
-  cout << endl << "Submatrix: " << endl;
+  cout << "Submatrix: " << endl;
   for(int number = 0; number < matrixSize; number++){
     if(number+number == matrixSize){
         cout<<endl;
     }
     cout << subMatrix[number] << " ";
   }
-  cout << endl;
 }
 
-// // Computes the average of an array of numbers
-// int computeSum(int* array, int num_elements) {
-//   int sum = 0;
-//   cout << "num_elements: " << num_elements << endl;
-//   cout << "numbers in array: ";
-//   for (int index = 0; index < num_elements; index++) {
-//     sum += array[index];
-//   }
-//   cout<< endl; 
-//   cout << "sum: " << sum << endl;
-//   return sum ;
-// }
+// Computes the average of an array of numbers
+int computeSum(int array[], int num_elements) {
+  int sum = 0;
+  cout << "num_elements: " << num_elements << endl;
+  cout << "numbers in array: ";
+  for (int index = 0; index < num_elements; index++) {
+    sum += array[index];
+  }
+  cout<< endl; 
+  cout << "sum: " << sum << endl;
+  return sum ;
+}
 
-void getSubMatrix(vector<vector <int> > &matrix, int startRow,int startCol ,int subMatrixSize){
-  int index;
-  int subMatrix[subMatrixSize+subMatrixSize];
+vector <int>getSubMatrix(vector<vector <int> > &matrix, int startRow,int startCol ,int subMatrixSize){
+  vector <int> subMatrix;
   int endCol = startCol+subMatrixSize;
   int endRow = startRow+subMatrixSize;
   
-  index = 0;
-  cout << "startRow: " << startRow << " startCol: "<< startCol <<endl;
-
   for (int row = startRow; row < endRow; row++){
-    cout<< "numbers being inserted: ";
     for(int col = startCol; col < endCol; col++){
-      cout << matrix[row][col] << ' ';
-      subMatrix[index] = matrix[row][col];
-      index++;
+      subMatrix.push_back(matrix[row][col]);
     }
     cout << endl;
   }
-  displaySubMatrix(subMatrix, subMatrixSize);
- }
+  //displaySubMatrix(subMatrix, subMatrixSize);
+  return subMatrix;
+}
+
+
 
 int main(int argc, char** argv) {
+  bool readyToSend;
   ifstream inputFile;
   int matrixWidth;
   int world_rank;
   int world_size;
+  int currentDestProcess = 1;
+  vector<int> subMatrix;
   string line;
 
   if (argc != 2){
@@ -109,59 +108,20 @@ int main(int argc, char** argv) {
     cout << "Sub Matrix list: "<< endl; 
     for(int row = 0; row < matrixWidth; row += sizeOfSubMatrix){
       for(int col = 0; col < matrixWidth; col += sizeOfSubMatrix){
-        getSubMatrix(matrix,row,col,sizeOfSubMatrix);
-        cout << endl << endl;
+        subMatrix = getSubMatrix(matrix,row,col,sizeOfSubMatrix);
+        displaySubMatrix(subMatrix, sizeOfSubMatrix);
+        // if(currentDestProcess == world_size){
+        //   currentDestProcess = 1;
+        // }
+        // MPI_Send( subMatrix, sizeOfSubMatrix, MPI_INT,currentDestProcess++,0, MPI_COMM_WORLD);
       }
       cout << endl;
     }
   }
   else{ 
-    
+    // MPI_Recv(&subMatrix, sizeOfSubMatrix, MPI_INT, 0, 0, MPI_COMM_WORLD,0);
+    //displaySubMatrix(subMatrix,sizeOfSubMatrix );
   }
- 
-  // // For each process, create a buffer that will hold a subset of the entire
-  // // array
-  // int **sub_rand_nums = (int **)malloc(sizeof(int) * num_elements_per_proc);
-  // assert(sub_rand_nums != NULL);
-  
-  // // Scatter the random numbers from the root process to all processes in
-  // // the MPI world
-  // MPI_Scatter(rand_nums, num_elements_per_proc, MPI_INT, sub_rand_nums,
-  //             num_elements_per_proc, MPI_INT, 0, MPI_COMM_WORLD);
-  // // Compute the average of your subset
-  // int sub_avg = compute_avg(sub_rand_nums, num_elements_per_proc);
-
-  // // Gather all partial averages down to the root process
-  // int **sub_avgs = NULL;
-  // if (world_rank == 0) {
-  //   sub_avgs = (int **)malloc(sizeof(int) * world_size);
-  //   assert(sub_avgs != NULL);
-  // }
-  // MPI_Gather(&sub_avg, 1, MPI_INT, sub_avgs, 1, MPI_INT, 0, MPI_COMM_WORLD);
-
-  // // Now that we have all of the partial averages on the root, compute the
-  // // total average of all numbers. Since we are assuming each process computed
-  // // an average across an equal amount of elements, this computation will
-  // // produce the correct answer.
-  // if (world_rank == 0) {
-  //   int sum = 0;
-  //   if( sum < sub_avg){
-  //     sum = sub_avg;
-  //   }
-
-  //   printf("max sum of all elements is %d\n", sum);
-  //   // Compute the average across the original data for comparison
-  //   int original_data_avg =
-  //     compute_avg(rand_nums, num_elements_per_proc * world_size);
-  //   printf("Max Sum computed across original data is %d\n", original_data_avg);
-  // }
-
-  // // Clean up
-  // if (world_rank == 0) {
-  //   free(rand_nums);
-  //   free(sub_avgs);
-  // }
-  // free(sub_rand_nums);
 
   MPI_Barrier(MPI_COMM_WORLD);
   MPI_Finalize();
