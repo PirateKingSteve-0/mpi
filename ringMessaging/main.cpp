374bec6810aa8101b0862b4d2f32e10c75108acc
Austin Suarez <amsuarez@outlook.com>
2018-12-26 22:47:30 -0800

    cleaning up ring topology message passing

--- ringMessaging/main.cpp
+++ ringMessaging/main.cpp
@@ -9,35 +9,45 @@ int main(int argc, char** argv){
    int world_rank;
    int world_size;
    int nextProc;
+   int prevProc;
    int tmp;
    
    MPI_Init(NULL, NULL); // initiates the communicator
    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank); // reports the rank, identifying the calling process
    MPI_Comm_size(MPI_COMM_WORLD, &world_size); // reports the number of processes
-   //tmp =  malloc(sizeof(int));
    
+   // establishes the next process that it will send a msg to.
    if(world_rank == world_size-1){
       nextProc = 0;
    }
    else{
       nextProc = world_rank+1;
    }
-   
+
+   // establishing the process it is recieving a msg from 
+   prevProc = world_rank-1;
+
+   // process 0 sends a message
    if(world_rank == 0){
       tmp = 1;
       cout << "Sending first message now..." << endl;
+      // sends a message to next process
       MPI_Send(&tmp, 1, MPI_INT,nextProc,0, MPI_COMM_WORLD);
-   }
-   else{      
-      MPI_Recv(&tmp, 1, MPI_INT, world_rank-1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-      MPI_Send(&tmp, 1, MPI_INT,nextProc,0, MPI_COMM_WORLD);
-   }
-   
-   if(world_rank == 0){
-      MPI_Recv(&tmp, 1, MPI_INT, world_size-1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+      // establishing the process it is recieving a msg from for process 0 
+      prevProc = world_size-1;
+      
+      // recieves another rank
+      MPI_Recv(&tmp, 1, MPI_INT, prevProc, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
       cout<< "Finally ";
    }
-
+   else{      
+      // recieves the message from the lower rank then sends to next process
+      MPI_Recv(&tmp, 1, MPI_INT, prevProc, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      tmp++;
+      MPI_Send(&tmp, 1, MPI_INT,nextProc,0, MPI_COMM_WORLD);
+   }
+   // outputs result
    cout << "rank " << world_rank << " says " << tmp << endl;
    MPI_Barrier(MPI_COMM_WORLD);
    MPI_Finalize(); 
